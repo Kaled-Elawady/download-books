c547af339b00fbb45ed1cd99c3366085c524f372
khaledelawady <82104020+khaledelawady1@users.noreply.github.com>
2023-10-26 16:07:48 +0300

    update logo and add js files

---
+++ js/school.js
@@ -0,0 +1,22 @@
+const books =[
+    /* 0 اللغة العربية */ "https://drive.google.com/uc?export=download&id=1WqXtXuEgBrTLqC4AwXNBO6WSiLnHXCma" ,
+    /* 1 اللغة الانجليزية */ "https://elearnningcontent.blob.core.windows.net/elearnningcontent/content/sec/semester1/Grade1/pdf/English-New%20Hello_Student%E2%80%99s%20Book%20-Sec1-T1.pdf" ,
+    /* 2 اللغة الفرنسية */ "https://drive.google.com/u/0/uc?id=11P9tc9-bgcYC5DLXK1-pyNqV4bUxue9_&export=download" ,
+    /* 3 الكيمياء */ "https://drive.google.com/uc?export=download&id=15jfd4ZZkv-z5ZKY3wTZMxRJAUSWwYmtw",
+    /* 4  الفيزياء */ "https://elearnningcontent.blob.core.windows.net/elearnningcontent/content/sec/semester1/Grade1/pdf/PHYSICS_1sec.pdf" ,
+    /* 5  الاحياء */ "https://elearnningcontent.blob.core.windows.net/elearnningcontent/content/sec/semester1/Grade1/pdf/biolgy_1sec.pdf" ,
+    /* 6  الجغرافيا */ "https://elearnningcontent.blob.core.windows.net/elearnningcontent/content/sec/semester1/Grade1/pdf/geographic_1sec.pdf" ,
+    /* 7  التاريخ */ "https://elearnningcontent.blob.core.windows.net/elearnningcontent/content/sec/semester1/Grade1/pdf/history_1sec_sb_wb.pdf" ,
+    /* 8 الرياضيات */ "https://drive.google.com/uc?export=download&id=1U_eo8B63P-7iGGIYcP_4SwsLVWz38UBH",
+    /* 9 التربية الدينية */ "https://elearnningcontent.blob.core.windows.net/elearnningcontent/content/sec/semester1/Grade1/pdf/deen_islamy_1sec_t1_t2.pdf" ,
+    /* 10 التربية الوطنية */ "https://elearnningcontent.blob.core.windows.net/elearnningcontent/content/sec/semester1/Grade1/pdf/trbia_wtanea_1sec.pdf",
+    /* 11 الكومبيوتر */ "https://books-library.net/files/books-library.online-08072030Rv0R0.pdf",
+    /* 12 معجزات القران */ "https://elearnningcontent.blob.core.windows.net/elearnningcontent/content/sec/semester1/Grade1/pdf/old/mogeza_elquran_1sec.pdf" ,
+    /* 13 بناء الشخصية الوطنية */ "https://elearnningcontent.blob.core.windows.net/elearnningcontent/content/sec/semester1/Grade1/pdf/bena_elshakhsia_1sec.pdf" ,
+
+]
+function openu(url){
+    win = window.open(url , "_blank")
+    win.focus()
+}
+
